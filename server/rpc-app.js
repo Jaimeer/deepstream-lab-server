f486a920de99944cb8c22a68d18461ec9110e97f
Jaime Escolar <jaime.escolar@gmail.com>
2017-02-13 19:55:00 +0100

    update collection name

--- server/rpc-app.js
+++ server/rpc-app.js
@@ -7,14 +7,14 @@ exports.init = () => {
     client.rpc.provide('addChat', function (data, response) {
         console.log('addChat', data)
         data.id = client.getUid()
-        const chatRecord = client.record.getRecord('chat/' + data.id)
+        const chatRecord = client.record.getRecord('chat-item/' + data.id)
         chatRecord.set(data)
         chatListData.addEntry(data.id)
         response.send('OK');
     });
     client.rpc.provide('removeChat', function (data, response) {
         console.log('removeChat', data)
-        const chatRecord = client.record.getRecord('chat/' + data)
+        const chatRecord = client.record.getRecord('chat-item/' + data)
         chatRecord.whenReady(() => {
             console.log('removeChat', 'READY')
             chatRecord.delete()
